207acb6393ecc27429128059faa464c8e0218b50
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-09-23 17:04:59 +0300

    Initial Commit

---
+++ MaxCard/MaxCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxCardApp.swift
+//  MaxCard
+//
+//  Created by Maxim Bekmetov on 23.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct MaxCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
