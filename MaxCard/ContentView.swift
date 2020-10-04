4e47bb275f38b9a072b08b24cf802b16e41e55ec
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-10-04 15:44:58 +0300

    Business Card
    swiftUI

--- MaxCard/ContentView.swift
+++ MaxCard/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00).edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Maxim_bekmetov")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 110, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 3))
+                Text("Maxim Bekmetov")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+7 963 978 66 66", imageName: "phone.fill")
+                InfoView(text: "maxim_bekmetov@protonmial.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +38,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ MaxCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  MaxCard
+//
+//  Created by Maxim Bekmetov on 04.10.2020.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 40)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName).foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
